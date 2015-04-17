01b04ad55451b2d53a5db76e65e9dcdda97cf7b7
Ananthu R V <ananthurv666@hotmail.com>
2015-04-17 17:52:14 +0530

    [mohit/ananthu] created the form for taking the feed back in

--- app/scripts/controllers/index.js
+++ app/scripts/controllers/index.js
@@ -1,17 +1,7 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name feedbackUiApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the feedbackUiApp
- */
 angular.module('feedbackUiApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('IndexCtrl', function ($scope) {
+    $scope.feedback = '';
+    $scope.author = '';
+});
